c54cf5edf00803da1b61babee9604a0b5c156b0a
Chris Bowers <pcbowers@gmail.com>
2023-02-25 17:55:26 -0500

    build: 🚨 Improve eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,18 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
+  root: true,
   ignorePatterns: ["**/*.cjs"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:@typescript-eslint/strict",
+    "plugin:@typescript-eslint/strict"
   ],
+  plugins: ["@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["./tsconfig.json"],
+    project: true,
+    tsConfigRootDir: __dirname
   },
-  root: true,
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/ban-types": "off",
-    "@typescript-eslint/no-this-alias": "off",
-  },
-  overrides: [
-    {
-      files: ["**/*.test.ts"],
-      rules: {
-        "@typescript-eslint/dot-notation": "off",
-      },
-    },
-  ],
+  rules: {}
 }
